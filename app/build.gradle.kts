d0533ce538ca439b09835c5cd43feaff9b0de003
benzovvozh <augusthoffman3@gmail.com>
2025-02-26 16:13:49 +0500

    try to connect db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,8 @@ repositories {
 application { mainClass.set("hexlet.code.App") }
 
 dependencies {
+    implementation("com.h2database:h2:2.2.220")
+    implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("io.javalin:javalin:6.1.3")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:6.1.3")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,17 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class App {
@@ -14,7 +20,27 @@ public class App {
         return Integer.valueOf(port);
     }
 
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    private static String getDBurl() {
+        String url = System.getenv()
+                .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        return url;
+    }
+
     public static Javalin getApp() throws IOException, SQLException {
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(getDBurl());
+        var dataSource = new HikariDataSource(hikariConfig);
+        var sql = readResourceFile("urls.sql");
+
+        log.info(sql);
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
@@ -28,6 +54,6 @@ public class App {
 
     public static void main(String[] args) throws SQLException, IOException {
         Javalin app = getApp();
-        app.start(7070);
+        app.start(getPort());
     }
 }
